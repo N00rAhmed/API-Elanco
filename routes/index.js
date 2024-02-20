9ee1e4745de1ec074fbc02cd531a24b344e40ff7
ELias020Ali <elias02ali@hotmail.com>
2024-02-20 14:28:04 +0000

    added more queries for avg  heartrate and avg hearttosleep

--- routes/index.js
+++ routes/index.js
@@ -62,4 +62,60 @@ router.get("/average_caninethree", function (req, res, next) {
     res.status(500).json({ error: "Internal Server Error" });
   });
 });
+
+router.get("/HeartRate_CanineOne", function (req, res, next) {
+  const HeartRate_CanineOne = req.query; // Corrected to use 'HeartRate_CanineOne'
+  sql.HighAndLowHeartRate_One(HeartRate_CanineOne).then((result) => {
+    res.json(result);
+  }).catch((error) => {
+    // Handle the error appropriately
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
+});
+
+router.get("/HeartRate_CanineTwo", function (req, res, next) {
+  const HeartRate_CanineTwo = req.query; // Corrected to use 'HeartRate_CanineOne'
+  sql.HighAndLowHeartRate_Two(HeartRate_CanineTwo).then((result) => {
+    res.json(result);
+  }).catch((error) => {
+    // Handle the error appropriately
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
+});
+
+router.get("/HeartRate_CanineThree", function (req, res, next) {
+  const HeartRate_CanineThree = req.query; // Corrected to use 'HeartRate_CanineOne'
+  sql.HighAndLowHeartRate_Three(HeartRate_CanineThree).then((result) => {
+    res.json(result);
+  }).catch((error) => {
+    // Handle the error appropriately
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
+});
+
+router.get("/HeartToSleep_CanineOne", function (req, res, next) {
+  const HeartToSleep_CanineOne = req.query; // Corrected to use 'HeartRate_CanineOne'
+  sql.HeartRateToSleep_One(HeartToSleep_CanineOne).then((result) => {
+    res.json(result);
+  }).catch((error) => {
+    // Handle the error appropriately
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
+});
+
+router.get("/HeartToWalk_CanineOne", function (req, res, next) {
+  const HeartToWalk_CanineOne = req.query; // Corrected to use 'HeartRate_CanineOne'
+  sql.HeartRateToWalk_One(HeartToWalk_CanineOne).then((result) => {
+    res.json(result);
+  }).catch((error) => {
+    // Handle the error appropriately
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
+});
+
 module.exports = router;
